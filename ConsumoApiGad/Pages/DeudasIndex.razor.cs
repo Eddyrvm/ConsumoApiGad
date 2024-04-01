196974a0f826f95e638d6d0b5549bf3566507a52
Eddy Vera Mejia <nayo_ec@outlook.com>
2024-04-01 09:46:48 -0500

    MEJORAS A LA APLICACION

--- ConsumoApiGad/Pages/DeudasIndex.razor.cs
+++ ConsumoApiGad/Pages/DeudasIndex.razor.cs
@@ -1,6 +1,6 @@
 ﻿using ConsumoApiGAD.Models;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ConsumoApiGad.Pages
 {
@@ -40,103 +40,103 @@ namespace ConsumoApiGad.Pages
             listRubro.Clear();
             listComp.Clear();
             MostrarTabla = false;
-    }
+        }
 
-    private async Task SubmitFormAsync()
-    {
-        IsLoading = true;
-        try
+        private async Task SubmitFormAsync()
         {
-            using (var httpClient = new HttpClient())
+            IsLoading = true;
+            try
             {
-                var json = await httpClient.GetStringAsync($"https://api.gadbolivar.gob.ec/api/public/deudas?criterio=CE&valor={ValorB}");
-
-                object? task = JsonConvert.DeserializeObject(json.ToString());
-                JObject obj = JObject.Parse(task!.ToString()!);
-                JValue _fechaHora = (JValue)obj["fechaHora"]!;
-                JArray _persona = (JArray)obj["personas"]!;
-                JArray _componentes = (JArray)obj["componentes"]!;
-                FechaHora = (string)obj["fechaHora"]!;
-                foreach (var item in _persona)
+                using (var httpClient = new HttpClient())
                 {
-                    Personas = new Personas
-                    {
-                        NombresApellidos = (string)item.SelectToken("apellidosNombres")!,
-                        Identificador = (string)item.SelectToken("identificador")!,
-                        Direccion = (string)item.SelectToken("direccion")!,
-                        Email = (string)item.SelectToken("email")!,
-                        Id = (int)item.SelectToken("id")!,
-                        Nuevo = (bool)item.SelectToken("nuevo")!,
-                        Rural = (bool)item.SelectToken("rural")!,
-                        Telefono = (string)item.SelectToken("telefono")!,
-                    };
-                }
+                    var json = await httpClient.GetStringAsync($"https://api.gadbolivar.gob.ec/api/public/deudas?criterio=CE&valor={ValorB}");
 
-                foreach (var itemComp in _componentes)
-                {
-                    Componente = new Componente
+                    object? task = JsonConvert.DeserializeObject(json.ToString());
+                    JObject obj = JObject.Parse(task!.ToString()!);
+                    JValue _fechaHora = (JValue)obj["fechaHora"]!;
+                    JArray _persona = (JArray)obj["personas"]!;
+                    JArray _componentes = (JArray)obj["componentes"]!;
+                    FechaHora = (string)obj["fechaHora"]!;
+                    foreach (var item in _persona)
                     {
-                        NombreMiembro = (string)itemComp.SelectToken("nombreMiembro")!,
-                        Valor = (decimal)itemComp.SelectToken("valor")!,
-                    };
-                    listComp.Add(Componente);
-                }
-
-                if (_componentes!.Count > 0)
-                {
-                    foreach (var y in _componentes)
-                    {
-                        JArray _titulos = (JArray)y["titulos"]!;
-
-                        foreach (var i in _titulos)
+                        Personas = new Personas
                         {
-                            Titulo = new Titulo
-                            {
-                                ClavePredia = (string)i.SelectToken("clavePredial")!,
-                                FechaEmision = (string)i.SelectToken("fechaEmision")!,
-                                Year = (string)i.SelectToken("year")!,
-                                ValorNeto = (string)i.SelectToken("valorNeto")!,
-                                ValorTitulo = (string)i.SelectToken("valorTitulo")!,
-                                ValorEmision = (string)i.SelectToken("valorEmision")!,
-                            };
+                            NombresApellidos = (string)item.SelectToken("apellidosNombres")!,
+                            Identificador = (string)item.SelectToken("identificador")!,
+                            Direccion = (string)item.SelectToken("direccion")!,
+                            Email = (string)item.SelectToken("email")!,
+                            Id = (int)item.SelectToken("id")!,
+                            Nuevo = (bool)item.SelectToken("nuevo")!,
+                            Rural = (bool)item.SelectToken("rural")!,
+                            Telefono = (string)item.SelectToken("telefono")!,
+                        };
+                    }
 
-                            JArray _rubros = (JArray)i["rubros"]!;
-                            JObject _intereses = (JObject)i["interes"]!;
-                            JValue nombreIntereses = (JValue)_intereses["nombreRubro"]!;
-                            JValue valorIntereses = (JValue)_intereses["valor"]!;
-                            JObject _emision = (JObject)i["emision"]!;
-                            JValue nombreEmision = (JValue)_emision["nombreRubro"]!;
-                            JValue valorEmision = (JValue)_emision["total"]!;
-                            foreach (var item in _rubros)
+                    foreach (var itemComp in _componentes)
+                    {
+                        Componente = new Componente
+                        {
+                            NombreMiembro = (string)itemComp.SelectToken("nombreMiembro")!,
+                            Valor = (decimal)itemComp.SelectToken("valor")!,
+                        };
+                        listComp.Add(Componente);
+                    }
+
+                    if (_componentes!.Count > 0)
+                    {
+                        foreach (var y in _componentes)
+                        {
+                            JArray _titulos = (JArray)y["titulos"]!;
+
+                            foreach (var i in _titulos)
                             {
-                                Rubro = new Rubro
+                                Titulo = new Titulo
                                 {
-                                    NombreRubro = (string)item.SelectToken("nombreRubro")! + " Periodo: " + (string)i.SelectToken("periodo")!,
-                                    Valor = (decimal)item.SelectToken("valor")!,
-                                    ValorTotalInteres = (decimal)_intereses.SelectToken("total")!,
-                                    GastosAdministrativos = (decimal)_emision.SelectToken("total")!,
-                                    Total = (decimal)item.SelectToken("total")!,
-
+                                    ClavePredia = (string)i.SelectToken("clavePredial")!,
+                                    FechaEmision = (string)i.SelectToken("fechaEmision")!,
+                                    Year = (string)i.SelectToken("year")!,
+                                    ValorNeto = (string)i.SelectToken("valorNeto")!,
+                                    ValorTitulo = (string)i.SelectToken("valorTitulo")!,
+                                    ValorEmision = (string)i.SelectToken("valorEmision")!,
                                 };
-                                listRubro.Add(Rubro);
+
+                                JArray _rubros = (JArray)i["rubros"]!;
+                                JObject _intereses = (JObject)i["interes"]!;
+                                JValue nombreIntereses = (JValue)_intereses["nombreRubro"]!;
+                                JValue valorIntereses = (JValue)_intereses["valor"]!;
+                                JObject _emision = (JObject)i["emision"]!;
+                                JValue nombreEmision = (JValue)_emision["nombreRubro"]!;
+                                JValue valorEmision = (JValue)_emision["total"]!;
+                                foreach (var item in _rubros)
+                                {
+                                    Rubro = new Rubro
+                                    {
+                                        NombreRubro = (string)item.SelectToken("nombreRubro")! + " Periodo: " + (string)i.SelectToken("periodo")!,
+                                        Valor = (decimal)item.SelectToken("valor")!,
+                                        ValorTotalInteres = (decimal)_intereses.SelectToken("total")!,
+                                        GastosAdministrativos = (decimal)_emision.SelectToken("total")!,
+                                        Total = (decimal)item.SelectToken("total")!,
+
+                                    };
+                                    listRubro.Add(Rubro);
+                                }
                             }
                         }
                     }
-                }
 
 
-                else
-                {
-                    // Manejar la situación en la que no se recibe ninguna respuesta
+                    else
+                    {
+                        // Manejar la situación en la que no se recibe ninguna respuesta
+                    }
                 }
+
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            IsLoading = false;
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        IsLoading = false;
     }
-}
 }
